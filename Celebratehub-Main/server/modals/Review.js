2dd23d5ddc9e2d0586c045dea7cfdb29411c1c7b
afaanlulu <afaanlulu@gmail.com>
2026-05-10 13:36:07 +0000

    checkpoint, leave reviwe works

--- Celebratehub-Main/server/modals/Review.js
+++ Celebratehub-Main/server/modals/Review.js
@@ -30,4 +30,6 @@ const ReviewSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('Review', ReviewSchema);
+const Review = mongoose.model("Review", ReviewSchema);
+
+module.exports = Review;
